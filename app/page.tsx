1e01a44b59f4e25ce50ab2235e379c420cf10674
Dean <dsangyeop@gmail.com>
2025-08-05 04:53:50 +0900

    Add global Header component

--- app/page.tsx
+++ app/page.tsx
@@ -305,7 +305,8 @@ export default function Home() {
 
             {/* <div className="col-span-2 p-12 flex flex-col justify-center bg-white h-full overflow-y-auto"> */}
             {/* <div className="col-span-2 p-12 flex flex-col justify-center bg-white overflow-y-auto"> */}
-            <div className="col-span-2 p-12 flex flex-col justify-center bg-white h-screen">
+            {/* <div className="col-span-2 p-12 flex flex-col justify-center bg-white h-screen"> */}
+            <div className="col-span-2 p-12 flex flex-col justify-center bg-white">
 
               {/*회사개요*/}
               <div className="border-b border-gray-200 pb-2">
